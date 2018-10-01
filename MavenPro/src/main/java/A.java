66af61d860edd4c81d3a74a4316a109d8c15d3ae
Karn <shruti.karn@capgemini.com>
2018-10-01 12:22:14 +0530

    added

--- MavenPro/src/main/java/A.java
+++ MavenPro/src/main/java/A.java
@@ -1,8 +1,17 @@
 
-public class A {
+public class A{
+	public void print(){
+	B bean=new B();
+	bean.name="Shruti";
+	bean.id=101;
+	System.out.println("my name is "+bean.name+" and my id is "+bean.id);
+	}
 public static void main (String[] args){
+	
 	System.out.println("hello!");
+	
 }
+
 }
 
 
---
+++ MavenPro/src/main/java/B.java
@@ -0,0 +1,24 @@
+
+public class B {
+
+	public int id;
+	public String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
+
+
